7c494a32e9a3eec85fa9d67a69f324cfc7b2b8c5
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-05-01 19:58:00 +0530

    First JavaScript Code and Learning start

---
+++ first.js
@@ -0,0 +1,19 @@
+console.log("RCB - IPL 2026 Champion");
+console.log("RCB - IPL 2025 Champion");
+console.log("Adarsh Mishra");
+
+// ------------------------
+
+King = "Virat Kohli";
+console.log(King);
+HighestScore = 973;
+console.log(HighestScore);
+Duck = null;
+console.log(Duck);
+RetirementYear = undefined;
+console.log(RetirementYear);
+Choker = false;
+console.log(Choker);
+
+fan1 = "Adarsh";
+console.log(fan1);
