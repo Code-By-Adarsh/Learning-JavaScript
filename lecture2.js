e794f651eb25edca430ff695d917817906a85a93
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-05-04 17:51:38 +0530

    Add: Lecture 2

---
+++ lecture2.js
@@ -0,0 +1,21 @@
+// Comments
+console.log("Comments in JS - Parts of code which not runs");
+// single line comment
+
+/*
+Multiple line comments 
+*/
+
+// Operators
+// Arithmetic Operator
+// + or Increment
+let a = 7;
+let b = 5;
+let c = a + b;
+console.log("a = ",a,"& b = ",b);
+console.log("a + b = ",c);
+console.log("a - b = ",a - b);
+console.log("a * b = ",a * b);
+console.log("a / b = ",a / b);
+console.log("a % b = ",a % b); // Modulus operator -> Remainder
+console.log("a ** b = ",a ** b); // Exponentiation operator
