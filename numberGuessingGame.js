2f3ea9d69494b69a81c3177cb02d688f1c4b4acf
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-05-20 10:54:42 +0530

    Number Guessing Game

---
+++ numberGuessingGame.js
@@ -0,0 +1,12 @@
+alert("-----Welcome to Number Guessing Game-----")
+const answer = 7;
+let turn = 1;
+do{
+    if(turn >= 2){
+        var userNumber = prompt("Chance: "+turn+" [You entered wrong number] Enter your number between 1 and 10: ");
+    }else{
+        var userNumber = prompt("Chance: "+turn+" Enter your number between 1 and 10: ");
+    }
+    turn++;
+}while(userNumber != answer);
+console.log("ye you won");
