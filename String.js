1a13aeea851e5fcfe3d7bb9606b07f25840c65f1
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-05-24 09:26:09 +0530

    Built in methods

--- String.js
+++ String.js
@@ -28,3 +28,12 @@ console.log(`The cost of 1 ${obj.item} is ${obj.price} rupees.`)
 // \n --> for new line
 // \t --> tab space
 console.log("Adarsh \nMishra");
+
+//Methods --> These are the built in methods to manipulate a string
+let str = " Radhe Radhe";
+let upperStr = str.toUpperCase();
+console.log(upperStr); // for all capital letter
+console.log(str.toLowerCase()); // for all smaller letter
+console.log(str.trim()); // for removing whitespace for starting and ending
+console.log(str.slice(1,6)); // return part of string end part is inclusive
+
